34c2b7a842e01505d6fe14653d187edbe2896df9
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-06 18:47:11 +0000

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
 	}
-	else if (rem > 5)
+	else if (rem >= 6)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
 	}
