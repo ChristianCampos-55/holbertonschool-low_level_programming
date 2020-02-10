88ccb353df66faa65dd567bcb5e8491c89ca1666
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-10 22:54:04 +0000

    1

---
+++ 0x02-functions_nested_loops/.#1-main.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.2428:1581357565
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+int main(void)
+{
+	int x;
+	int v;
+
+	while (v < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			putchar(x);
+		}
+		putchar('\n');
+		v++;
+	}
+}
