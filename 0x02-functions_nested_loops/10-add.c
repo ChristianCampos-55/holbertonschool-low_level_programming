47e7ad750a14e905b0a319516d9a9dc7de99e05c
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-11 19:29:56 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @a: first digit to add
+ * @b: second digit to add
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
