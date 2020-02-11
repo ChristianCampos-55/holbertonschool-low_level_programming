910dff3dd5ddaea27e082e3f66843370ac4bac6a
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-11 16:48:06 +0000

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _abs - check the code for Holberton School students.
+ * @n: number to receive
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		n = (n * -1);
+		return (n);
+	}
+
+	else
+	{
+		return (n);
+	}
+}
