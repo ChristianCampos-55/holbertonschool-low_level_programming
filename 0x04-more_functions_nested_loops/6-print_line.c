b1e4aace3829c448e862c9108aca1bb2b3cf16d2
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-13 18:38:58 +0000

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,14 @@
 
 /**
  * print_line - check the code for Holberton School students.
- * @n:lines to print
+ * @n: lines to print
  *
  * Return: Always 0.
  */
 
 void print_line(int n)
 {
-	int a;
+	int a = 0;
 
 	while (a < n)
 	{
