2a6b111581c7e91f695f967cae19f62f1acd41c4
WinDave <gamochris@gmail.com>
2020-02-21 03:36:57 +0000

    adding 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,17 +7,18 @@
  */
 char *rot13(char *string)
 {
-char norm[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char rot[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char norm[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 int a, b;
 
-for (a = 0; s[a] != '\0'; a++)
+for (a = 0; string[a] != '\0'; a++)
 {
 for (b = 0; b < 52; b++)
 {
 if (string[a] == norm[b])
 {
 string[a] = rot[b];
+break;
 }
 }
 }
