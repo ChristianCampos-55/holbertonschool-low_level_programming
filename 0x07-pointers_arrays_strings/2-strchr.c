ba6bf7e6231e20a32a17af1356edd5c3177f27b4
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-24 17:33:51 +0000

    ex 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - find a specific character in a string.
+ * @s: string to look in.
+ * @c: character to find in string.
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		s++;
+		if (c == s[count])
+		{
+			return (s);
+		}
+		if (!*s)
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
