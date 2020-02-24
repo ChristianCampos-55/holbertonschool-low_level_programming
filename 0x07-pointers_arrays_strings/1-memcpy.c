bdd28967c58a6dfb88e97e69e750bc6a2480a11e
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-24 17:47:43 +0000

    fixing 0, 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (count = 0; count < n; count++)
 	{
-		src[count] = dest[count];
+		*(src + count) = *(dest + count);
 	}
 	return (dest);
 }
