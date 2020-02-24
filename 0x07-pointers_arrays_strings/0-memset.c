1f493cda5429011e91532206bb8bf6b841dcc02c
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-24 16:19:27 +0000

    ex 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * char *memset - assigns a char to a pointed adress
+ * @s: the address of memory to print to
+ * @b: the address of memory to print from
+ * @n: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count <= n; count++)
+	{
+		s[count] = b;
+	}
+	return (s);
+}
