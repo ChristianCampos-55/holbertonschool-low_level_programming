f24e232dc95a6bc1fbfbb28551eb45228306cf8d
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-24 17:35:27 +0000

    ex 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * char *memset - assigns a char to a pointed adress
+ * char *_memset - assigns a char to a pointed adress
  * @s: the address of memory to print to
  * @b: the address of memory to print from
  * @n: the size of the memory to print
