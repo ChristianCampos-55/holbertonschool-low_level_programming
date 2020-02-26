a2e7cafc1644061b0f065bbd2ee3aa7afe538323
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-26 20:24:09 +0000

    adding 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - measure the lenght of a string.
+ * @s: string to measure.
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
