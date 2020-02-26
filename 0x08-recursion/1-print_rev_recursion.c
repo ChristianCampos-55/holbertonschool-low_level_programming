d181310a6141439e32f1cd09cda6d64002f76ffe
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-26 19:17:54 +0000

    folder

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse.
+ * @s: string to reverse.
+ * Return: 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	{
+		_print_rev_recursion(s + 1);
+	}
+
+	putchar(s);
+}
