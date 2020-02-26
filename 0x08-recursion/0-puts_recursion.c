9a92052bd7f07fe854446968459c3621d4064fde
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-26 19:45:44 +0000

    h and 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - prints a string
- * @s: string to print
- * Return: 0.
+ * _puts_recursion - prints a string.
+ * @s: string to print.
+ * Return: Always 0.
  */
+
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
 	if (*s != '\0')
 	{
+		_putchar (*s);
 		s++;
 		_puts_recursion(s);
+	}
+	else if (*s == '\0')
+	{
+		_putchar ('\n');
 	}
 }
