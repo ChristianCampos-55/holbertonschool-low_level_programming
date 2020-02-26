d1b342d7dc4e365e2f03c16a0b06d53c28f946a8
ChristianCampos-55 <1566@holbertonschool.com>
2020-02-26 19:17:03 +0000

    0 + h

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to print
+ * Return: 0.
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s != '\0')
+	{
+		s++;
+		_puts_recursion(s);
+	}
+	else if (*s == '\0')
+	{
+		_putchar ('\n');
+	}
+}
