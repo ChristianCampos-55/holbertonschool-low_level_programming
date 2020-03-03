e9b5413721293d82276602a7b7be7fb4da224bf0
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-03 22:35:27 +0000

    adding 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings in one.
+ * @s1: string 1.
+ * @s2: string 2;
+ * Return: return pointer or Null if fails.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *sum;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	while (s1[a])
+		a++;
+
+	while (s2[b])
+		b++;
+
+	sum = malloc(((a + 1) + (b + 1)) * sizeof(char));
+
+	if (sum == NULL)
+		return (NULL);
+
+	a = 0;
+	b = 0;
+
+	while (s1[a])
+	{
+		sum[c] = s1[a];
+		a++;
+		c++;
+	}
+	sum[c] = ' ';
+
+	while (s2[b])
+	{
+		sum[c] = s2[b];
+		b++;
+		c++;
+	}
+	sum[c] = '\0';
+
+	return (sum);
+}
