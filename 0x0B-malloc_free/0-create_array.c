6f095dc76e846e0575711b40ca0643c9b39e4222
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-03 19:37:39 +0000

    adding folder and ex 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - function to create an array
+ * @size: size of the array.
+ * @c: character to print in first index.
+ * Return: array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a = 0;
+	char *array;
+
+	array = (char *)malloc(size * sizeof(char));
+
+	for (a = 0; a < size; a++)
+	{
+		if (size == 0)
+		{
+			return (NULL);
+		}
+		array[a] = c;
+	}
+	return (array);
+}
