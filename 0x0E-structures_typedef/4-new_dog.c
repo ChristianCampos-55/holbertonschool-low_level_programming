f34fd7f828f7992be312caaf523018e94290fbf4
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-09 22:22:30 +0000

    adding 4 again

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
 /**
  * new_dog - creates a new dog.
  * @name: creates dog's name.
@@ -12,14 +11,44 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *namenew;
+	char *ownernew;
 	dog_t *ndog;
+
+	int a, b;
+
+	for (a = 0; name[a]; a++)
+	{}
+
+	namenew = malloc((a + 1) * sizeof(char));
+	if (namenew == NULL)
+		return (NULL);
+
+	for (b = 0; b <= a; b++)
+	{
+		namenew[b] = name[b];
+	}
+	namenew[b] = '\0';
+
+	for (a = 0; owner[a]; a++)
+	{}
+
+	ownernew = malloc((a + 1) * sizeof(char));
+	if (ownernew == NULL)
+		return (NULL);
+
+	for (b = 0; b <= a; b++)
+	{
+		ownernew[b] = owner[b];
+	}
+	ownernew[b] = '\0';
 
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
 
-	ndog->name = name;
+	ndog->name = namenew;
 	ndog->age = age;
-	ndog->owner = owner;
+	ndog->owner = ownernew;
 	return (ndog);
 }
