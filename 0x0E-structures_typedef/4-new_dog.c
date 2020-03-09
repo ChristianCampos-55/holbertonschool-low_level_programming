6cee9e52d4f5c205daff6a06d146d9a7be59f63d
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-09 22:42:11 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,39 +14,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *namenew;
 	char *ownernew;
 	dog_t *ndog;
-
 	int a, b;
 
 	for (a = 0; name[a]; a++)
 	{}
-
 	namenew = malloc((a + 1) * sizeof(char));
 	if (namenew == NULL)
 		return (NULL);
-
 	for (b = 0; b < a; b++)
-	{
 		namenew[b] = name[b];
-	}
 	namenew[b] = '\0';
-
 	for (a = 0; owner[a]; a++)
 	{}
-
 	ownernew = malloc((a + 1) * sizeof(char));
 	if (ownernew == NULL)
-		return (NULL);
-
-	for (b = 0; b < a; b++)
 	{
-		ownernew[b] = owner[b];
+		free(namenew);
+		return (NULL);
 	}
+	for (b = 0; b < a; b++)
+		ownernew[b] = owner[b];
 	ownernew[b] = '\0';
 
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
+	{
+		free(namenew);
+		free(ownernew);
 		return (NULL);
-
+	}
 	ndog->name = namenew;
 	ndog->age = age;
 	ndog->owner = ownernew;
