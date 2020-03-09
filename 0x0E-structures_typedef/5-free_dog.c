f6f1e156c16b4b74f919dffd79d56533cf5b129d
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-09 23:42:29 +0000

    adding ex 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - func to free struc dog.
+ * @dog_t: struct dog to free.
+ * @d: pointer to struct.
+ *
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}
