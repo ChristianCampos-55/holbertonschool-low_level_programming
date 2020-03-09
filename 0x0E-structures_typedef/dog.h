9981330965d0dea1bd6b556e97dab16ee4c6fa6f
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-09 18:44:40 +0000

    correcting folder name

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates a struct of type dog.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: name of owner.
+ *
+ * Description: struct that takes dog info and assigns it to a struct.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
