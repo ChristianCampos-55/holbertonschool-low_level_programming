b0ce2734911dd1a3baa266b20777e0861601e26a
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-12 19:23:15 +0000

    adding files for ex 3 again

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,27 +10,27 @@
 
 int main(int argc, char *argv[])
 {
-int x, y, z;
+	int x, y, z;
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-&& argv[2][0] != '/' && argv[2][0] != '%')
-{
-printf("Error\n");
-exit(99);
-}
-if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
-{
-printf("Error\n");
-exit(100);
-}
-x = atoi(argv[1]);
-y = atoi(argv[3]);
-z = get_op_func(argv[2])(x, y);
-printf("%i\n", z);
-return (0);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+	     && argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	z = get_op_func(argv[2])(x, y);
+	printf("%i\n", z);
+	return (0);
 }
