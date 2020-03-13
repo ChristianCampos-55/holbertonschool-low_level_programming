16129731b42ad498b0826b5fceaa294bdffd4bb9
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-13 17:53:51 +0000

    adding ex 3 so far

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	
+}
