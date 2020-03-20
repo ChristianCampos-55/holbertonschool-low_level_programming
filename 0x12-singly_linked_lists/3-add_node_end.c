bf2b07c52c6e39d5071c6187935d03decb65b712
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-20 18:39:19 +0000

    [ADD]: Adding a parenthesis that the malloc was lacking

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,7 +18,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (head == NULL)
 		return (NULL);
 
-	new = (malloc(sizeof(list_t));
+	new = (malloc(sizeof(list_t)));
 
 	if (new == NULL)
 		return (NULL);
