96c5847bab8dd6057a4217e94b7d83c04977d4c1
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-24 19:05:59 +0000

    [ADD]: Ex 5; [UPDATED]: lists.h.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint.
+ * @head: pointer to adress of list to free.
+ *
+ * Return: Always 0.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	while (*head)
+	{
+		aux = *head;
+		*head = aux->next;
+		free(aux);
+	}
+}
