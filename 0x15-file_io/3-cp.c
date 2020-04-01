00c5061610ef012e7fd47eedd5fcc0910e2695c3
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-01 03:16:56 +0000

    [ADD] Ex 3.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @ac: number of arguments passed.
+ * @av: vector of arguments passed.
+ *
+ * Return: 0.
+ */
+
+int main(int ac, char **av)
+{
+	int file_from = 0, file_to = 0, written = 0, red = 0;
+	char buff[1024];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			av[1]), exit(98);
+	file_to = open(av[2], O_TRUNC | O_CREAT | O_RDWR, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
+			av[2]), exit(99);
+	red = read(file_from, buff, 1024);
+	if (red == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			av[1]), exit(98);
+	written = write(file_to, buff, red);
+	if (written == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
+			av[2]), exit(99);
+	close(file_from);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", av[1]);
+		exit(100);
+	close(file_to);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", av[2]);
+		exit(100);
+	return (0);
+}
