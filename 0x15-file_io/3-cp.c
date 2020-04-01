7babab8cb1c2dbd047f527fe553977acaa65c18b
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-01 03:51:13 +0000

    [FIX] Fixing fd closing errors.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,10 +7,10 @@
  *
  * Return: 0.
  */
-
 int main(int ac, char **av)
 {
-	int file_from = 0, file_to = 0, written = 0, red = 1024;
+	int file_from = 0, file_to = 0, written = 0, red = 0,
+		temp = 0;
 	char buff[1024];
 
 	if (ac != 3)
@@ -37,13 +37,13 @@ int main(int ac, char **av)
 				"Error: Can't write to file %s\n",
 				av[2]), exit(99);
 	}
-	close(file_from);
-	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", av[1]);
+	temp = close(file_from);
+	if (temp == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
 		exit(100);
-	close(file_to);
-	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", av[2]);
+	temp = close(file_to);
+	if (temp == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
 		exit(100);
 	return (0);
 }
