5cba1318bf1c0f55437b394f80c1fcd7f55ddefd
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-03 21:35:54 +0000

    [FIX] Linking order

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int cntr = NULL;
-	dlistint_t *dexer = (*h);
+	dlistint_t *dexer = *h;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
@@ -20,8 +20,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!(*h))
 	{
-		*(h) = new_node;
+		new_node->prev = NULL;
 		new_node->next = NULL;
+		*(h) = new_node;
 		return (new_node);
 	}
 
@@ -31,11 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (cntr = 0; cntr < idx; cntr++)
 		dexer = dexer->next;
 
-	dexer = dexer->prev;
-	(dexer->prev)->next = new_node;
-	new_node->next = dexer->next;
 	new_node->prev = dexer->prev;
-	(dexer->next)->prev = new_node;
+	(dexer->prev)->next = new_node;
+	dexer->prev = new_node;
+	new_node->next = dexer;
 	new_node->n = n;
 	return (new_node);
 }
