322a5a902dbe2bd34938cc2af3af54d52253b7c4
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-03 13:59:37 +0000

    [FIX] Change counter location

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 /**
  * print_dlistint - prints all elements of a dlistint_t list.
- * @h: head to DLL
+ * @h: head to DLL.
  *
- * Return: EXIT_SUCCESS, EXIT_FAILURE on failure.
+ * Return: nodes.
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
+
+	if (!h)
+		return (nodes);
 
 	while(h)
 	{
