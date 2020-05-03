51ee2123bbf4639a0f5639879022114457eac384
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-03 22:54:04 +0000

    [ADD] Ex 8, lists

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Delete a specific node of a DLL.
+ * @head: DP to DLL.
+ * @index: node to delete.
+ *
+ * Return: 1 if success, -1 if you are a waste of a programmer.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+	unsigned int countr;
+	dlistint_t *dummy = (*head);
+
+	if (!(*head))
+		return (0);
+
+	if (index == 0 && (*head)->next != NULL)
+	{
+		(*head) = (*head)->next;
+		(*head)->prev = NULL;
+		return (1);
+	}
+
+	else if ((*head)->next == NULL)
+	{
+		(*head) = NULL;
+		return (1);
+	}
+
+	else
+	{
+		for (countr = 0; countr < index; countr++)
+		{
+			dummy = dummy->next;
+		}
+
+		(dummy->next)->prev = dummy->prev;
+		(dummy->prev)->next = dummy->next;
+		return (1);
+	}
+
+}
